25713c7cd0564f40248154db60c407557b076ff4
Martin Keller <martin.keller@taketool.de>
2025-04-16 11:25:57 +0200

    [Mod1] fix getPages4PluginType() for non-existing index 0

--- Classes/Controller/Mod1Controller.php
+++ Classes/Controller/Mod1Controller.php
@@ -1070,7 +1070,13 @@ class Mod1Controller extends ActionController
                 // This could be improved by handing in a Context object and decide whether hidden pages
                 // Should be linkeable too
             }
-            $siteRoot = (!empty($rootLineArray[0]['title'])) ? $rootLineArray[0]['title'] : '* no title *';
+
+            // why can this be empty?
+            if (empty($rootLineArray[0])) continue;
+
+            $siteRoot = (!empty($rootLineArray[0]['title']))
+                ? $rootLineArray[0]['title']
+                : '* no title *';
             unset($rootLineArray[0]);
 
             $rLTemp = [];
